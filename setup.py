c9b0252b61211835c3db84531180333faaf8529b
Vishnu Mohan <vishnu@mesosphere.com>
2018-07-17 20:14:47 -0400

    Remove jupyter from requirements

--- setup.py
+++ setup.py
@@ -137,7 +137,6 @@ setup(
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=['ipython >= 6',
-                      'jupyter',
                       'notebook >= 5',
                       'beautifulsoup4',
                       'widgetsnbextension',
